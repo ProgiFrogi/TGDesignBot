bb8dba1c65657644d1838cbc6d82095d8bc5e42e
Kira_Andr <andreev.ka@phystech.edu>
2024-04-13 19:00:57 +0300

    added drop scripts

---
+++ DBHandler/drop_scripts.py
@@ -0,0 +1,22 @@
+import psycopg2
+from config import load_config
+
+
+def drop_tables():
+    """ Drop All tables in the database"""
+    commands = (
+        """drop table if exists fonts;""",
+        """drop table if exists images;""",
+        """drop table if exists slides""",
+        """drop table if exists users""",
+        """drop table if exists templates"""
+    )
+    try:
+        config = load_config()
+        with psycopg2.connect(**config) as conn:
+            with conn.cursor() as cur:
+                # execute the CREATE TABLE statement
+                for command in commands:
+                    cur.execute(command)
+    except (psycopg2.DatabaseError, Exception) as error:
+        print(error)
