93ac3a8237869b93f896f486a1d7629a772e9e7d
Kira_Andr <andreev.ka@phystech.edu>
2024-07-27 11:53:33 +0300

    fixed comments

--- TelegramHandler/handlers/global_args/global_args_namespace.py
+++ TelegramHandler/handlers/global_args/global_args_namespace.py
@@ -1,9 +1,11 @@
 import pickle
 
-# try:
-tree_obj = pickle.load(open("./Tree/ObjectTree.pkl", "rb"))
-# except:
-#     # tree = ClassTree.Tree()
+from TGDesignBot.Tree import ClassTree
+
+try:
+    tree_obj = pickle.load(open("./Tree/ObjectTree.pkl", "rb"))
+except:
+    tree = ClassTree.Tree()
 dist_indx = 9
 indx_list_start = 0
 indx_list_end = indx_list_start + dist_indx
